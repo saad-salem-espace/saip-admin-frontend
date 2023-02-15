1237ab4a88102fb448d3cdae3e7bad943f3d2b09
omnia yahia <omnia.yehia@espace.com>
2023-02-15 16:55:46 +0000

    IP-206: search input in search resuts page

--- src/components/search-results/SearchResults.jsx
+++ src/components/search-results/SearchResults.jsx
@@ -2,10 +2,16 @@ import { Formik, Form } from 'formik';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { useTranslation } from 'react-i18next';
 import SearchResultCard from './search-result-card/SearchResultCard';
 import SearchNote from './SearchNote';
+import Select from '../shared/form/select/Select';
+import Search from '../shared/form/search/Search';
+// import formStyle from '../shared/form/form.module.scss';
 
 function SearchResults() {
+  const { t } = useTranslation('search');
+
   const results = [
     {
       id: 1,
@@ -28,10 +34,62 @@ function SearchResults() {
       abstract: 'I claim: 1. A railroad freight car truck having a load rating, said truck comprising: a bolster, sideframes, spring groups and wheelsets; said bolster being mounted cross-wise to said...',
     },
   ];
+  const options = [
+    {
+      key: '1',
+      value: 'any field',
+    },
+    {
+      key: '2',
+      value: 'Int. Classification(IPC)',
+    },
+  ];
+
+  const onChangeSelect = () => {
+
+  };
+
+  const onSubmit = () => {
+
+  };
+
+  const WorkStreamsOptions = [
+    {
+      key: '1',
+      value: 'patents',
+    },
+    {
+      key: '2',
+      value: 'copy right',
+    },
+  ];
+
+  const onSelectWorkStream = () => {
+
+  };
+
   return (
     <Container fluid className="px-0">
       <Row className="mx-0">
-        <Col lg={4} md={6}>
+        <Col md={{ span: 10, offset: 1 }} className="mb-8">
+          <Formik>
+            {() => (
+              <Form className="mt-8">
+                <div className="d-flex align-items-stretch">
+                  <div className="d-flex align-items-center">
+                    <h4 className="mb-0">Search</h4>
+                    <Select options={WorkStreamsOptions} onChangeSelect={onSelectWorkStream} id="workStreams" fieldName="workStreams" moduleClassName="custom-select" className="me-5 ms-3" />
+                  </div>
+                  <div className="position-relative">
+                    <Select options={options} onChangeSelect={onChangeSelect} id="searchFields" fieldName="searchFields" moduleClassName="sm-select" />
+                  </div>
+                  <Search id="search" className="flex-grow-1" moduleClassName="sm-search" placeholder={t('typeSearchTerms')} onSubmit={onSubmit} />
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+        <Col lg={4} md={6} className="mx-12">
           <SearchNote searchKeywords="Title: “car” AND Publication date: “11/11/2020 AND IPC: ”A61K”" resultsCount={50} />
           <Formik>
             {() => (
