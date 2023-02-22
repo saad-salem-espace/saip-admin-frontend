2c6cfed47ad423e62f4d5744156a3cb0a9bd2599
John Eskandar <john.eskandar@espace.com.eg>
2023-02-22 02:12:49 +0200

    IP-207: add functionality to search results

--- src/components/search-results/SearchResults.jsx
+++ src/components/search-results/SearchResults.jsx
@@ -2,45 +2,42 @@ import { Formik, Form } from 'formik';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import SearchResultCard from './search-result-card/SearchResultCard';
+import { useSearchParams } from 'react-router-dom';
+import useWorkstreams from 'hooks/useWorkstreams';
 import SearchNote from './SearchNote';
+import AppPagination from '../shared/app-pagination/AppPagination';
+import SearchResultCards from './search-result-cards/SearchResultCards';
 
 function SearchResults() {
-  const results = [
-    {
-      id: 1,
-      title: 'Rail road car and truck therefor',
-      priority: '2001-1-02',
-      filingNumber: '20-30',
-      publicationNumber: 'US US10745034B2',
-      filingDate: '1',
-      publishedAt: '20-2-2022',
-      abstract: 'I claim: 1. A railroad freight car truck having a load rating, said truck comprising: a bolster, sideframes, spring groups and wheelsets; said bolster being mounted cross-wise to said...',
-    },
-    {
-      id: 2,
-      title: 'Rail road car and truck therefor',
-      priority: '2001-1-02',
-      filingNumber: '20-30',
-      publicationNumber: 'US US10745034B2',
-      filingDate: '1',
-      publishedAt: '20-2-2022',
-      abstract: 'I claim: 1. A railroad freight car truck having a load rating, said truck comprising: a bolster, sideframes, spring groups and wheelsets; said bolster being mounted cross-wise to said...',
-    },
-  ];
+  const [searchParams] = useSearchParams();
+  const searchResultParams = {
+    workstreamId: searchParams.get('workstreamId'),
+    identifierStrId: searchParams.get('identifierStrId'),
+    queryString: searchParams.get('query'),
+  };
+  const { getIdentifierByStrId, isReady } = useWorkstreams(searchResultParams.workstreamId);
+  if (!isReady) return null;
+
+  const identifier = getIdentifierByStrId(searchParams.get('identifierStrId'));
+
   return (
     <Container fluid className="px-0">
       <Row className="mx-0">
         <Col lg={4} md={6}>
-          <SearchNote searchKeywords="Title: “car” AND Publication date: “11/11/2020 AND IPC: ”A61K”" resultsCount={50} />
+          <SearchNote
+            searchKeywords={`${identifier}: “${searchResultParams.queryString}”`}
+            resultsCount={50}
+          />
           <Formik>
             {() => (
               <Form className="mt-8">
-                {
-                  results.map((searchResult) => (
-                    <SearchResultCard key={searchResult.id} searchResult={searchResult} />
-                  ))
-                }
+                <AppPagination
+                  axiosConfig={{
+                    url: 'search',
+                    params: searchResultParams,
+                  }}
+                  RenderedComponent={SearchResultCards}
+                />
               </Form>
             )}
           </Formik>
