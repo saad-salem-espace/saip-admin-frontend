07a104e1ab42c816e0d20dfb3d98557ccc693758
John Eskandar <john.eskandar@espace.com.eg>
2023-02-23 21:55:37 +0200

    IP-207: improve search result components and add highlighting

--- src/components/search-results/search-result-cards/search-result-card/SearchResultCard.jsx
+++ src/components/search-results/search-result-cards/search-result-card/SearchResultCard.jsx
@@ -2,25 +2,40 @@ import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { useSearchParams } from 'react-router-dom';
 import { trimStringRelativeToSubtext } from 'utils/strings';
 import Button from 'components/shared/button/Button';
 import Checkbox from 'components/shared/form/checkboxes/checkbox/Checkbox';
+import Highlighter from 'react-highlight-words';
 import style from './style.module.scss';
 
-function SearchResultCard({ searchResult }) {
+function SearchResultCard({ searchResult, query }) {
   const { t } = useTranslation('search');
-  const [searchParams] = useSearchParams();
   const { BibliographicData } = searchResult;
 
   return (
     <div className={`${style['result-card']} mb-7 position-relative px-1`}>
       <div className="d-flex align-items-start mb-1">
         <Checkbox className="mt-1" />
-        {/* please wrap the search keyword with <span className="font-bold"></span> */}
-        <Button variant="link" className="text-start f-20 pe-0 py-0 text-primary-dark" text={BibliographicData.ApplicationTitle} />
+        <Button
+          variant="link"
+          className="text-start f-20 pe-0 py-0 text-primary-dark"
+          text={(
+            <Highlighter
+              highlightTag="span"
+              highlightClassName="font-bold"
+              textToHighlight={trimStringRelativeToSubtext(
+                BibliographicData.ApplicationTitle,
+                query,
+              )}
+              searchWords={[query]}
+              autoEscape
+            />
+          )}
+        />
       </div>
-      <p className="mb-2">{BibliographicData.PublicationNumber}</p>
+      <p className="mb-2">
+        {BibliographicData.PublicationNumber}
+      </p>
       <p className="font-medium mb-2 d-lg-flex align-items-center">
         {t('priority', { value: searchResult.Priority })}
         <FontAwesomeIcon icon={faCircle} className="mx-1 f-8" />
@@ -34,12 +49,17 @@ function SearchResultCard({ searchResult }) {
           )
         }
       </p>
-      {/* please wrap the search keyword with <span className="font-bold"></span> */}
       <p className="text-gray sm-text">
-        {trimStringRelativeToSubtext(
-          BibliographicData.ApplicationAbstract.join(' '),
-          searchParams.get('query'),
-        )}
+        <Highlighter
+          highlightTag="span"
+          highlightClassName="font-bold"
+          textToHighlight={trimStringRelativeToSubtext(
+            BibliographicData.ApplicationAbstract.join(' '),
+            query,
+          )}
+          searchWords={[query]}
+          autoEscape
+        />
       </p>
     </div>
   );
@@ -57,6 +77,7 @@ SearchResultCard.propTypes = {
     }),
     Priority: PropTypes.string.isRequired,
   }).isRequired,
+  query: PropTypes.string.isRequired,
 };
 
 export default SearchResultCard;
