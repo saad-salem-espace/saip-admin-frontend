453fcddc47466c80aeaab774d51edb347ad7f734
omnia yahia <omnia.yehia@espace.com>
2023-03-06 16:54:41 +0000

    IP-664: date range style

--- src/components/shared/date-picker/AppDatePicker.jsx
+++ src/components/shared/date-picker/AppDatePicker.jsx
@@ -1,15 +1,24 @@
-import './DatePicker.scss';
 import DatePicker from 'react-multi-date-picker';
+import './DatePicker.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays } from '@fortawesome/free-regular-svg-icons';
+import PropTypes from 'prop-types';
 
-function AppDatePicker() {
+function AppDatePicker({ range }) {
   return (
     <div className="datePicker position-relative">
-      <DatePicker />
+      <DatePicker range={range} />
       <FontAwesomeIcon icon={faCalendarDays} className="calendar-icon f-20 text-primary" />
     </div>
   );
 }
+
+AppDatePicker.propTypes = {
+  range: PropTypes.bool,
+};
+
+AppDatePicker.defaultProps = {
+  range: false,
+};
 
 export default AppDatePicker;
