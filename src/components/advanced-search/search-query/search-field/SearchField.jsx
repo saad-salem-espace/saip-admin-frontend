ff19786c0d1b825f6a3d49137e3a0ed227a430db
omnia yahia <omnia.yehia@espace.com>
2023-03-08 16:31:23 +0000

    IP-664: error massage & style

--- src/components/advanced-search/search-query/search-field/SearchField.jsx
+++ src/components/advanced-search/search-query/search-field/SearchField.jsx
@@ -2,15 +2,16 @@
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+// import MultiSelect from 'components/shared/multi-select/MultiSelect';
 import formStyle from '../../../shared/form/form.module.scss';
 import Select from '../../../shared/form/select/Select';
 import style from '../SearchQuery.module.scss';
 import Button from '../../../shared/button/Button';
-import Input from '../../../shared/form/input/Input';
-import DatePicker from '../../../shared/date-picker/AppDatePicker';
+// import Input from '../../../shared/form/input/Input';
+// import DatePicker from '../../../shared/date-picker/AppDatePicker';
 
 function SearchField() {
-  const { t } = useTranslation('search');
+  const { t } = useTranslation(['search', 'translation']);
   const searchFieldsOptions = [
     {
       label: '1',
@@ -32,6 +33,12 @@ function SearchField() {
       value: '2',
     },
   ];
+
+  // const options = [
+  //   { label: 'Thing 1', value: 1 },
+  //   { label: 'Thing 2', value: 2 },
+  // ];
+
   return (
     <div className={`p-4 bg-primary-01 mb-2 ${style.wrapper}`}>
       <div className="d-flex mb-4">
@@ -56,22 +63,41 @@ function SearchField() {
           text={<FontAwesomeIcon icon={faTrashAlt} className="text-danger-dark" />}
         />
       </div>
-      <div className={`position-relative me-2 ${style.criteria}`}>
+      {/* <div className={`position-relative me-2 ${style.criteria}`}>
         <span className={`position-absolute ${formStyle.label}
          ${formStyle.smLabel}`}
         >
           {t('criteria')}
         </span>
         <Input moduleClassName="smInput" />
-      </div>
+      </div> */}
       {/* for datepicker */}
       {/* <div className={style.dateWrapper}>
-        <DatePicker />
+       please add error class if date has error
+        <DatePicker
+          className="error"
+          errorMsg={t('translation:noEmptyField')}
+        />
       </div> */}
       {/* for datepicker range */}
-      <div className={style.dateWrapper}>
-        <DatePicker range />
-      </div>
+      {/* please add error class if date has error */}
+      {/* <div className={style.dateWrapper}>
+        <DatePicker range className="error"
+        errorMsg={t('translation:noEmptyField')}
+/>
+      </div> */}
+      {/* <MultiSelect
+        options={options}
+        errorMsg={t('translation:noEmptyField')}
+        // please add class has-value if the user selects any option
+        // please add error class if select has error
+        className={`smMultiSelect ${style.advancedSearchSelect}`}
+        // please show the below label if the user selects any option
+        // label={t('publicationCountry')}
+      /> */}
+      {/* <div className="border-top mt-7 pb-2 pt-5">
+        <Button variant="link" className="font-regular px-0" text={t('clearAll')} />
+      </div> */}
     </div>
   );
 }
--- src/components/shared/date-picker/AppDatePicker.jsx
+++ src/components/shared/date-picker/AppDatePicker.jsx
@@ -3,22 +3,28 @@ import './DatePicker.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays } from '@fortawesome/free-regular-svg-icons';
 import PropTypes from 'prop-types';
+import ErrorMessage from '../error-message/ErrorMessage';
 
-function AppDatePicker({ range }) {
+function AppDatePicker({ range, className, errorMsg }) {
   return (
-    <div className="datePicker position-relative">
+    <div className={`datePicker position-relative ${className}`}>
       <DatePicker range={range} />
       <FontAwesomeIcon icon={faCalendarDays} className="calendar-icon f-20 text-primary" />
+      <ErrorMessage msg={errorMsg} className="mt-2 mb-0" />
     </div>
   );
 }
 
 AppDatePicker.propTypes = {
   range: PropTypes.bool,
+  className: PropTypes.string,
+  errorMsg: PropTypes.string,
 };
 
 AppDatePicker.defaultProps = {
   range: false,
+  className: '',
+  errorMsg: '',
 };
 
 export default AppDatePicker;
