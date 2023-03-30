516570b7b404bc61fb29057ec7c40992fe42e8a9
omnia yahia <omnia.yehia@espace.com>
2023-03-30 00:37:53 +0000

    IP-273: component for no data

--- src/components/shared/empty-states/HandleEmptyAttribute.jsx
+++ src/components/shared/empty-states/HandleEmptyAttribute.jsx
@@ -2,29 +2,25 @@ import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 const HandleEmptyAttribute = ({
-  checkOn, RenderedComponent, renderedProps, className, emptyComponent,
+  checkOn, RenderedComponent, renderedProps,
 }) => {
   const { t } = useTranslation();
   if (RenderedComponent && checkOn) {
     return <RenderedComponent {...renderedProps} />;
   }
-  return checkOn || <span className={` ${className ? '' : 'italicText text-gray'}`}>{emptyComponent ? t('noRelevantData') : t('emptyText')}</span>;
+  return checkOn || <i className="text-gray">{ t('emptyText')}</i>;
 };
 
 HandleEmptyAttribute.propTypes = {
   checkOn: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   RenderedComponent: PropTypes.func,
   renderedProps: PropTypes.instanceOf(Object),
-  className: PropTypes.string,
-  emptyComponent: PropTypes.bool,
 };
 
 HandleEmptyAttribute.defaultProps = {
   checkOn: '',
   RenderedComponent: null,
   renderedProps: {},
-  className: '',
-  emptyComponent: false,
 };
 
 export default HandleEmptyAttribute;
