abaf5b27086d79d36adc602edada0019c3a57b50
John Eskandar <john.eskandar@espace.com.eg>
2023-04-06 18:35:47 +0200

    IP-1149: change all apis to axios configuration

--- src/apis/search/advancedSearchApi.js
+++ src/apis/search/advancedSearchApi.js
@@ -1,20 +1,15 @@
-import axios from 'axios';
-
 const advancedSearchApi = ({
   workstreamId, query, imageName, enableSynonyms, page,
-}, configOnly = false) => {
-  const config = {
-    url: 'advanced-search',
-    method: 'GET',
-    params: {
-      workstreamId,
-      q: query,
-      ...(imageName && { imageName }),
-      ...(enableSynonyms && { enableSynonyms }),
-      page: page || 1,
-    },
-  };
-  return configOnly ? config : axios.request(config);
-};
+}) => ({
+  url: 'advanced-search',
+  method: 'GET',
+  params: {
+    workstreamId,
+    q: query,
+    ...(imageName && { imageName }),
+    ...(enableSynonyms && { enableSynonyms }),
+    page: page || 1,
+  },
+});
 
 export default advancedSearchApi;
