baa406f92b68fdec54c1aecda531589400232754
Nada Dahab <ndahab.c@saip.gov.sa>
2023-04-10 16:18:31 +0200

    IP-1527: fix-tests

--- src/components/search-results/SearchResults.jsx
+++ src/components/search-results/SearchResults.jsx
@@ -185,10 +185,6 @@ function SearchResults() {
   };
 
   useEffect(() => {
-    setSearchQuery(searchResultParams.query);
-    // eslint-disable-next-line
-    const regexPattern = new RegExp('true');
-    setIsEnabledSynonyms(regexPattern.test(searchParams.get('enableSynonyms')));
     if (searchIdentifiers) {
       const decodedQuery = decodeQuery(searchResultParams.query);
       setFlattenedCriteria(flattenCriteria(decodedQuery));
@@ -198,14 +194,18 @@ function SearchResults() {
         id: 1, data: '', identifier: searchIdentifiersData[0], condition: searchIdentifiersData[0].identifierOptions[0], operator: '',
       }]);
     }
-  }, [searchResultParams.query, searchIdentifiers, searchParams]);
+  }, [searchResultParams.query, searchIdentifiers]);
 
   useEffect(() => {
+    // eslint-disable-next-line
+    const regexPattern = new RegExp('true');
+    setIsEnabledSynonyms(regexPattern.test(searchParams.get('enableSynonyms')));
+    setSearchQuery(searchResultParams.query);
     const keywords = parseQuery(searchFields, searchParams.get('imageName'), false);
     if (keywords) {
       setSearchKeywords(keywords);
     }
-  }, [searchFields]);
+  }, [searchFields, searchParams, searchResultParams.query]);
 
   const resetSearch = (workstreamId) => {
     setActiveWorkstream(workstreamId.toString());
