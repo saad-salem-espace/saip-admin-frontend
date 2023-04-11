3dc6c523f06c45f8c0372c44c3e86cf2836361e0
omnia yahia <omnia.yehia@espace.com>
2023-04-11 12:27:11 +0000

    IP-1149: toast component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Routes from 'components/routes/Routes';
 import ThemeProvider from 'components/theme/ThemeProvider';
 import AppNavbar from 'components/layout/app-navbar/AppNavbar';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './assets/styles/common/toast.scss';
 
 // import ChangeLanguage from 'components/change-language/ChangeLanguage';
 
@@ -11,16 +13,26 @@ function App() {
   // const changeLang = () => {
   //   setLang(lang === 'ar' ? 'en' : 'ar');
   // };
+
   return (
     <ThemeProvider
       // lang={lang}
       // eslint-disable-next-line react/jsx-closing-bracket-location
     >
       <div className="app">
-        <ToastContainer />
         {/* <ChangeLanguage changeLang={changeLang} /> */}
         <Routes />
         <AppNavbar />
+        <ToastContainer
+          position="bottom-left"
+          autoClose={8000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover
+        />
       </div>
     </ThemeProvider>
 
