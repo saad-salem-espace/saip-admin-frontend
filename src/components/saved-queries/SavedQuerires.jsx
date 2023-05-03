9751a8a36dcb999e35ae98eb220a71fa8ca5677a
John Eskandar <john.eskandar@espace.com.eg>
2023-05-03 19:45:32 +0300

    IP-1847: add list saved queries for non logged in user

--- src/components/saved-queries/SavedQuerires.jsx
+++ src/components/saved-queries/SavedQuerires.jsx
@@ -1,36 +1,41 @@
 import { useTranslation } from 'react-i18next';
 import getSavedQueryApi from 'apis/save-query/getSavedQueryApi';
 import Select from 'components/shared/form/select/Select';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import AppPagination from 'components/shared/app-pagination/AppPagination';
 import NoData from 'components/shared/empty-states/NoData';
 import { useSearchParams } from 'react-router-dom';
+import { useAuth } from 'react-oidc-context';
+import { tableNames } from 'dbConfig';
+import CacheContext from 'contexts/CacheContext';
+import useCacheRequest from 'hooks/useCacheRequest';
+import { pascalCase } from 'change-case';
 import SavedQueriresTable from './SavedQueriresTable';
+import IndexedDbAppPagination from '../shared/app-pagination/IndexedDbAppPagination';
 
 const SavedQuerires = () => {
   const { t } = useTranslation('queries');
   const [selectedWorkStream, setSelectedWorkStream] = useState({ label: t('patent'), value: 1 });
   const [searchParams] = useSearchParams();
+  const auth = useAuth();
+  const { cachedRequests } = useContext(CacheContext);
+  const [workstreams] = useCacheRequest(cachedRequests.workstreams, { url: 'workstreams' });
 
-  const WorkStreamsOptions = [
-    {
-      label: t('patent'),
-      value: 1,
-    },
-    {
-      label: t('trademark'),
-      value: 2,
-    },
-  ];
+  const WorkStreamsOptions = workstreams?.data?.map((workstream) => ({
+    label: pascalCase(workstream.workstreamName),
+    value: workstream.id,
+  }));
 
   const onChangeWorkStream = (i) => {
     setSelectedWorkStream(i);
   };
 
   const axiosConfig = getSavedQueryApi(selectedWorkStream.value, true);
+
+  const isAuth = auth && auth.user;
 
   const savedQueries = (
     SavedQueriresTable
@@ -49,15 +54,32 @@ const SavedQuerires = () => {
               className="workStreams me-5 ms-3 mt-1 customSelect"
             />
           </div>
-          <AppPagination
-            axiosConfig={axiosConfig}
-            defaultPage={Number(searchParams.get('page') || '1')}
-            RenderedComponent={
+          {isAuth ? (
+            <AppPagination
+              axiosConfig={axiosConfig}
+              defaultPage={Number(searchParams.get('page') || '1')}
+              RenderedComponent={
               savedQueries
             }
-            emptyState={(
-              <NoData />)}
-          />
+              emptyState={(
+                <NoData />)}
+            />
+          ) : (
+            <IndexedDbAppPagination
+              RenderedComponent={savedQueries}
+              tableName={tableNames.savedQuery}
+              emptyState={<NoData />}
+              indexMethod="indexByIndexName"
+              indexMethodProps={{
+                sorted: 'desc',
+                sortedIndexName: 'updatedAt',
+                indexName: 'workstreamId',
+                indexValue: selectedWorkStream.value,
+              }}
+              updateDependencies={[selectedWorkStream]}
+            />
+          )}
+
         </Col>
       </Row>
     </Container>
