a441b374898fa2a7d232bb1c4c93b3d2ed7ddb94
sameh-moh <sameh.mohamed@espace.com.eg>
2023-05-10 14:16:46 +0000

    IP-1989-emptyStateNoWorkstreams

--- src/components/examiner-dashboard/ExaminerDashboard.jsx
+++ src/components/examiner-dashboard/ExaminerDashboard.jsx
@@ -2,50 +2,13 @@ import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import useAxios from 'hooks/useAxios';
 import getAssigned from 'apis/dashboard/getAssigned';
+import getAssignedWorkstreams from 'apis/dashboard/getAssignedWorkstreams';
 import Spinner from 'components/shared/spinner/Spinner';
 import Sidebar from './sidebar/Sidebar';
 import Board from './board/Board';
 
 function ExaminerDashboard() {
   const { t } = useTranslation('dashboard');
-  // const linksList = [
-  //   {
-  //     id: 1,
-  //     workStreamName: t('dashboard:workStream.patent'),
-  //     WorkStreamClass: 'ic-patent',
-  //     BoardName: 'dashboard:board.patent',
-  //   },
-  //   {
-  //     id: 2,
-  //     workStreamName: t('dashboard:workStream.trademark'),
-  //     WorkStreamClass: 'ic-trademark',
-  //     BoardName: 'dashboard:board.trademark',
-  //   },
-  //   {
-  //     id: 3,
-  //     workStreamName: t('dashboard:workStream.copyrights'),
-  //     WorkStreamClass: 'ic-copyrights',
-  //     BoardName: 'dashboard:board.copyrights',
-  //   },
-  //   {
-  //     id: 4,
-  //     workStreamName: t('dashboard:workStream.industrialDesign'),
-  //     WorkStreamClass: 'ic-industrial-design',
-  //     BoardName: 'dashboard:board.industrialDesign',
-  //   },
-  //   {
-  //     id: 5,
-  //     workStreamName: t('dashboard:workStream.plantVarieties'),
-  //     WorkStreamClass: 'ic-plant-varieties',
-  //     BoardName: 'dashboard:board.plantVarieties',
-  //   },
-  //   {
-  //     id: 6,
-  //     workStreamName: t('dashboard:workStream.integratedCircuits'),
-  //     WorkStreamClass: 'ic-integrated-circuits',
-  //     BoardName: 'dashboard:board.integratedCircuits',
-  //   },
-  // ];
   const linksList = [
     {
       id: 1,
@@ -53,20 +16,67 @@ function ExaminerDashboard() {
       WorkStreamClass: 'ic-patent',
       BoardName: 'dashboard:board.patent',
     },
+    {
+      id: 2,
+      workStreamName: t('dashboard:workStream.trademark'),
+      WorkStreamClass: 'ic-trademark',
+      BoardName: 'dashboard:board.trademark',
+    },
+    {
+      id: 3,
+      workStreamName: t('dashboard:workStream.industrialDesign'),
+      WorkStreamClass: 'ic-industrial-design',
+      BoardName: 'dashboard:board.industrialDesign',
+    },
+    {
+      id: 4,
+      workStreamName: t('dashboard:workStream.copyrights'),
+      WorkStreamClass: 'ic-copyrights',
+      BoardName: 'dashboard:board.copyrights',
+    },
+    {
+      id: 5,
+      workStreamName: t('dashboard:workStream.plantVarieties'),
+      WorkStreamClass: 'ic-plant-varieties',
+      BoardName: 'dashboard:board.plantVarieties',
+    },
+    {
+      id: 6,
+      workStreamName: t('dashboard:workStream.integratedCircuits'),
+      WorkStreamClass: 'ic-integrated-circuits',
+      BoardName: 'dashboard:board.integratedCircuits',
+    },
   ];
+
   const [activeWorkstream, setActiveWorkstream] = useState(linksList[0]);
   const [sort, setSort] = useState('Queue');
-  const [{ data }, execute] = useAxios(getAssigned({
+  const [{ data }, executeAssignmentData] = useAxios(getAssigned({
     workstreamId: activeWorkstream.id,
     sortBy: sort,
   }), { manual: true });
+  const [workstreamsData, executeWorkstreamData] = useAxios(
+    getAssignedWorkstreams(),
+    { manual: true },
+  );
+
   const [assignments, setAssignments] = useState(null);
   const [toggle, setToggle] = useState(false);
 
   const [activeDocument, setActiveDocument] = useState(null);
+  const [assignedWorkstreams, setAssignedWorkstreams] = useState([]);
 
   useEffect(() => {
-    execute();
+    executeWorkstreamData();
+  }, []);
+
+  useEffect(() => {
+    if (workstreamsData?.data) {
+      setAssignedWorkstreams(workstreamsData.data.data);
+    }
+  }, [workstreamsData]);
+
+  useEffect(() => {
+    executeAssignmentData();
   }, [activeWorkstream, sort, toggle]);
 
   useEffect(() => {
@@ -76,24 +86,38 @@ function ExaminerDashboard() {
     }
   }, [data]);
 
+  if (!assignments) return (<div className="d-flex justify-content-center mt-18"><Spinner /></div>);
+
+  const changeWorkstream = (i) => {
+    setActiveWorkstream(i);
+    setActiveDocument(null);
+  };
+
+  const DashboardView = (
+    <div>
+      <Sidebar
+        linksList={linksList}
+        setActiveWorkstream={changeWorkstream}
+        activeWorkstream={activeWorkstream}
+        assignedWorkstreams={assignedWorkstreams}
+      />
+      <Board
+        activeWorkstream={activeWorkstream}
+        setSort={setSort}
+        assignments={assignments}
+        setToggle={setToggle}
+        setActiveDocument={setActiveDocument}
+        activeDocument={activeDocument}
+      />
+    </div>
+  );
+
   return (
-    assignments
+    assignedWorkstreams.length
       ? <div>
-        <Sidebar
-          linksList={linksList}
-          setActiveWorkstream={setActiveWorkstream}
-          activeWorkstream={activeWorkstream}
-        />
-        <Board
-          activeWorkstream={activeWorkstream}
-          setSort={setSort}
-          assignments={assignments}
-          setToggle={setToggle}
-          setActiveDocument={setActiveDocument}
-          activeDocument={activeDocument}
-        />
+        {DashboardView}
         {/* eslint-disable-next-line react/jsx-closing-tag-location */}
-      </div> : <div className="d-flex justify-content-center mt-18"><Spinner /></div>
+      </div> : <div />
   );
 }
 
--- src/components/examiner-dashboard/sidebar/Sidebar.jsx
+++ src/components/examiner-dashboard/sidebar/Sidebar.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import './sidebar.scss';
 
-function Sidebar({ setActiveWorkstream, activeWorkstream, linksList }) {
+function Sidebar({
+  setActiveWorkstream, activeWorkstream, linksList, assignedWorkstreams,
+}) {
   return (
     <div className="dashboard-sidebar justify-content-center bg-white shadow position-fixed top-0 start-0  w-px-65 vh-100">
       <div className="nav d-flex flex-column">
         {linksList.map((button) => (
-          <AppTooltip
+          assignedWorkstreams.includes(button.id) && <AppTooltip
             placement="auto"
             tooltipId={button.WorkStreamClass}
             tooltipContent={button.workStreamName}
@@ -37,6 +39,7 @@ Sidebar.propTypes = {
     WorkStreamClass: PropTypes.string,
     BoardName: PropTypes.string,
   })).isRequired,
+  assignedWorkstreams: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default Sidebar;
