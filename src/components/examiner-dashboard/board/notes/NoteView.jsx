053c86025bd2a050fd72f72783f8a60b259d9ffb
sameh-moh <sameh.mohamed@espace.com.eg>
2023-05-14 11:09:11 +0300

    IP-1999:fix-Name

--- src/components/examiner-dashboard/board/notes/NoteView.jsx
+++ src/components/examiner-dashboard/board/notes/NoteView.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/no-danger */
 import UserAvatar from 'components/shared/user-avatar/UserAvatar';
 import PropTypes from 'prop-types';
-import { useAuth } from 'react-oidc-context';
 import { calculateDifference } from 'utils/dates';
 import { useTranslation } from 'react-i18next';
 import EditAndDeleteMenu from './edit-and-delete/EditAndDeleteMenu';
@@ -10,15 +9,14 @@ import EditAndDeleteMenu from './edit-and-delete/EditAndDeleteMenu';
 function NoteView({
   note, setNotesUpdated, resetNotes, setActiveNote,
 }) {
-  const auth = useAuth();
   const { t } = useTranslation('notes');
   return (
     <div className="bg-white rounded p-6 shadow-sm mb-4">
       <div className="d-flex justify-content-between">
         <div className="d-flex align-items-center mb-4 me-2">
-          <UserAvatar name={auth.user?.profile.preferred_username} size="32" className="xs-text me-4" />
+          <UserAvatar name={note.user?.preferredUsername} size="32" className="xs-text me-4" />
           <h6 className="mb-0 text-gray-700 me-4">
-            {auth.user?.profile.preferred_username}
+            {note.user?.preferredUsername}
           </h6>
           <p className="text-gray mb-0 fs-sm">
             {calculateDifference(note.createdAt)}
