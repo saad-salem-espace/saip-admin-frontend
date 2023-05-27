50b6a9c9d67e37f71eaf1b038ca82710e87066d2
omnia yahia <omnia.yehia@espace.com>
2023-05-27 11:36:44 +0000

    IP-2431: remove classname

--- src/components/ipr-details/shared/seacrh-query/SearchQueryMenu.jsx
+++ src/components/ipr-details/shared/seacrh-query/SearchQueryMenu.jsx
@@ -71,7 +71,7 @@ function SearchQueryMenu({
   }, [searchIdentifiers]);
 
   return (
-    <div className={`search-query-wrapper ${className}`}>
+    <div className={`${className}`}>
       {children}
       {
         showSearchQuery && (
