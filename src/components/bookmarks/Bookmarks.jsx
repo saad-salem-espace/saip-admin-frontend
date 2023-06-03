2ff4bd4c23836d8fe0860e96ef4919ab2bde967e
sameh-moh <sameh.mohamed@espace.com.eg>
2023-06-03 06:29:44 +0000

    IP-2485:listWS

--- src/components/bookmarks/Bookmarks.jsx
+++ src/components/bookmarks/Bookmarks.jsx
@@ -5,17 +5,31 @@ import NoData from 'components/shared/empty-states/NoData';
 import SearchResultCards from 'components/search-results/search-result-cards/SearchResultCards';
 import TrademarksSearchResultCards from 'components/search-results/trademarks-search-result-cards/TrademarksSearchResultCards';
 import IndustrialDesignResultCards from 'components/search-results/industrial-design/IndustrialDesignResultCards';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import IprDetails from 'components/ipr-details/IprDetails';
 import { Formik } from 'formik';
+import i18n from 'i18next';
+import useCacheRequest from 'hooks/useCacheRequest';
+import CacheContext from 'contexts/CacheContext';
 
 const Bookmarks = () => {
+  const currentLang = i18n.language;
   const [searchParams] = useSearchParams();
   const axiosConfig = getBookmarksApi('1', 0, Number(searchParams.get('page') || '1'), true);
   const [activeDocument, setActiveDocument] = useState(null);
   const [isIPRExpanded, setIsIPRExpanded] = useState(false);
   const [results, setResults] = useState(null);
+  const { cachedRequests } = useContext(CacheContext);
+  const [workstreams] = useCacheRequest(cachedRequests.workstreams, { url: 'workstreams' });
 
+  function workstreamName(workstream) {
+    return currentLang === 'ar' ? workstream.workstreamNameAr : workstream.workstreamName;
+  }
+  const WorkStreamsOptions = workstreams?.data?.map((workstream) => ({
+    label: workstreamName(workstream),
+    value: workstream.id,
+  }));
+  console.log(WorkStreamsOptions);
   const getNextDocument = () => {
     if (!results || !activeDocument) return null;
     const index = results.findLastIndex(
