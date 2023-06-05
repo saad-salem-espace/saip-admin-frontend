f467ea2f1aa0c0bde33273cc5a8dd575ce0f5d6e
Islam Abdelzaher <ibdelzaher.c@saip.gov.sa>
2023-06-05 12:38:23 +0300

    IP-2485: bookmarks ui

--- src/components/bookmarks/BookmarkList.jsx
+++ src/components/bookmarks/BookmarkList.jsx
@@ -1,4 +1,6 @@
 import getBookmarksApi from 'apis/bookmarks/getBookmarksApi';
+import { useTranslation } from 'react-i18next';
+import Select from 'components/shared/form/select/Select';
 import { useSearchParams } from 'react-router-dom';
 import AppPagination from 'components/shared/app-pagination/AppPagination';
 import NoData from 'components/shared/empty-states/NoData';
@@ -14,8 +16,10 @@ import CacheContext from 'contexts/CacheContext';
 import IndexedDbAppPagination from 'components/shared/app-pagination/IndexedDbAppPagination';
 import useAuth from 'hooks/useAuth';
 import { tableNames } from 'dbConfig';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const BookmarkList = () => {
+  const { t } = useTranslation();
   const currentLang = i18n.language;
   const [searchParams] = useSearchParams();
   const axiosConfig = getBookmarksApi('1', 0, Number(searchParams.get('page') || '1'), true);
@@ -33,7 +37,7 @@ const BookmarkList = () => {
     label: workstreamName(workstream),
     value: workstream.id,
   }));
-  console.log(WorkStreamsOptions);
+  // console.log(WorkStreamsOptions);
   const getNextDocument = () => {
     if (!results || !activeDocument) return null;
     const index = results.findLastIndex(
@@ -83,62 +87,81 @@ const BookmarkList = () => {
   const isAuth = auth && auth.user;
 
   return (
-    <Formik>
-      <>
-        <h3>Bookmarks</h3>
-        {isAuth ? <AppPagination
-          className="mt-8"
-          axiosConfig={axiosConfig}
-          defaultPage={Number(searchParams.get('page') || '1')}
-          RenderedComponent={searchResult[1]}
-          emptyState={<NoData />}
-          bookmarks
-          bookmarkData={prepareBookmarks}
-          setResults={setResults}
-          renderedProps={{
-            selectedView,
-            setActiveDocument,
-            activeDocument,
-          }}
-        />
-          : <IndexedDbAppPagination
+    <Container fluid>
+      <Row className="px-md-19">
+        <Col lg={12}>
+          <div className="d-flex my-8 p-8 app-bg-primary-01 rounded">
+            <h5 className="mb-0 mt-4">{t('layout:navbar.myBookmarks')}</h5>
+            <Select
+              options={WorkStreamsOptions}
+              moduleClassName="menu"
+              // selectedOption={selectedWorkStream}
+              // setSelectedOption={onChangeWorkStream}
+              className="workStreams ms-3 mt-1 customSelect"
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row className="px-md-19">
+        <Col lg={7} className={`${isIPRExpanded ? 'd-none' : 'd-block'}`}>
+          <Formik>
+            {isAuth ? <AppPagination
               className="mt-8"
-              tableName={tableNames.bookmarks}
-              RenderedComponent={searchResult[1]}
-              indexMethod="indexByIndexName"
+              axiosConfig={axiosConfig}
               defaultPage={Number(searchParams.get('page') || '1')}
-              workstreamId="1"
+              RenderedComponent={searchResult[1]}
               emptyState={<NoData />}
-              indexMethodProps={{
-                sorted: 'desc',
-                sortedIndexName: 'updatedAt',
-                indexName: 'workstreamId',
-                indexValue: '1',
-              }}
+              bookmarks
+              bookmarkData={prepareBookmarks}
+              setResults={setResults}
               renderedProps={{
                 selectedView,
                 setActiveDocument,
                 activeDocument,
               }}
-              bookmarks
-          />}
-        {activeDocument && (
-        <div className="position-relative">
-          <IprDetails
-            collapseIPR={collapseIPR}
-            isIPRExpanded={isIPRExpanded}
-            documentId={activeDocument}
-            onClose={handleCloseIprDetail}
-            fromFocusArea={false}
-            setActiveDocument={setActiveDocument}
-            activeWorkstream={1}
-            getPreviousDocument={getPreviousDocument}
-            getNextDocument={getNextDocument}
-          />
-        </div>
-        )}
-      </>
-    </Formik>
+            />
+              : <IndexedDbAppPagination
+                  className="mt-8"
+                  tableName={tableNames.bookmarks}
+                  RenderedComponent={searchResult[1]}
+                  indexMethod="indexByIndexName"
+                  defaultPage={Number(searchParams.get('page') || '1')}
+                  workstreamId="1"
+                  emptyState={<NoData />}
+                  indexMethodProps={{
+                    sorted: 'desc',
+                    sortedIndexName: 'updatedAt',
+                    indexName: 'workstreamId',
+                    indexValue: '1',
+                  }}
+                  renderedProps={{
+                    selectedView,
+                    setActiveDocument,
+                    activeDocument,
+                  }}
+                  bookmarks
+              />}
+          </Formik>
+        </Col>
+        <Col lg={isIPRExpanded ? 12 : 5} className={`${isIPRExpanded ? '' : 'border-start'}`}>
+          {activeDocument && (
+            <div className="position-relative">
+              <IprDetails
+                collapseIPR={collapseIPR}
+                isIPRExpanded={isIPRExpanded}
+                documentId={activeDocument}
+                onClose={handleCloseIprDetail}
+                fromFocusArea={false}
+                setActiveDocument={setActiveDocument}
+                activeWorkstream={1}
+                getPreviousDocument={getPreviousDocument}
+                getNextDocument={getNextDocument}
+              />
+            </div>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
