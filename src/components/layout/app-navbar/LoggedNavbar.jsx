d826249690110f4a734cbcbeeb22598d3e41e912
Nada Dahab <ndahab.c@saip.gov.sa>
2023-06-12 13:15:02 +0300

    LoggedNavbar.jsx edited online with Bitbucket

--- src/components/layout/app-navbar/LoggedNavbar.jsx
+++ src/components/layout/app-navbar/LoggedNavbar.jsx
@@ -13,7 +13,7 @@ import React, {
 import getHistoryApi from 'apis/history/getHistoryApi';
 import useAxios from 'hooks/useAxios';
 import SelectedWorkStreamIdContext from 'contexts/SelectedWorkStreamIdContext';
-// import Notifications from './notifications/Notifications';
+import Notifications from './notifications/Notifications';
 import useAuth from '../../../hooks/useAuth';
 import LanguageSwitch from './shared/LanguageSwitch';
 import RecentSearch from './shared/recent-search/RecentSearch';
@@ -125,7 +125,7 @@ function LoggedNavbar({
             <Accessibility />
             <div className="d-flex justify-content-center h-px-39">
               {/* Notifications */}
-              {/* <Notifications /> */}
+              <Notifications />
               {/* Switch language */}
               <LanguageSwitch
                 className="pe-lg-5 me-lg-5"
